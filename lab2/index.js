cf49b164489d06cf47c13bf33c862429736450d6
vladmoroca <vladmoroca@gmail.com>
2024-04-23 22:27:35 +0300

    lab2

---
+++ lab2/index.js
@@ -0,0 +1,36 @@
+let request = require("request");
+
+let access_token = ''
+
+let options = { method: 'POST',
+    url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id":"dG9RzB82qEj9lSYCjgsGUWikomA1mmUc",\
+        "client_secret":"POkuM9El6ORqQXTShZBnC6dmhNifWvEPUDPGE2Zbqfe1waWcqMmXeNgfFo74nwO6",\
+        "audience":"https://dev-mpo4a74t24cr54of.us.auth0.com/api/v2/",\
+        "grant_type":"client_credentials"}' };
+
+let create_user = (token) => {
+    request({
+        method: 'POST',
+        url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/api/v2/users',
+        headers: {
+          'content-type': 'application/json',
+          'authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            email: 'example3@example.com',
+            password: 'very_Strong1_password',
+            connection: 'Username-Password-Authentication'
+        })
+    }, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        console.log(body);
+      });
+}
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    access_token = JSON.parse(body)['access_token'];
+    create_user(access_token)
+});
