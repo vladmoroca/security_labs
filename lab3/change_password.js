1d082e49da9ed8df5e3a2447219ddaded41b341a
vladmoroca <vladmoroca@gmail.com>
2024-04-27 17:26:35 +0300

    lab3

---
+++ lab3/change_password.js
@@ -0,0 +1,33 @@
+let request = require("request");
+
+
+let options = { method: 'POST',
+    url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id":"dG9RzB82qEj9lSYCjgsGUWikomA1mmUc",\
+        "client_secret":"POkuM9El6ORqQXTShZBnC6dmhNifWvEPUDPGE2Zbqfe1waWcqMmXeNgfFo74nwO6",\
+        "audience":"https://dev-mpo4a74t24cr54of.us.auth0.com/api/v2/",\
+        "grant_type":"client_credentials"}' };
+
+let change_password = (newPassword, token) => {
+    request({
+        method: 'PATCH',
+        url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/api/v2/users/auth0|66280ab4aaab3de91b5653f2',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            password: `${newPassword}`,
+            connection: 'Username-Password-Authentication'})
+        }, function (error, response, body) {
+        if (error) throw new Error(error);
+            console.log(JSON.parse(body));
+        });
+    };
+    
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    token = JSON.parse(body)['access_token'];
+    change_password("New_STRONG2_password", token)
+});
---
+++ lab3/index.js
@@ -0,0 +1,39 @@
+let request = require("request");
+
+let user_token_options = { method: 'POST',
+  url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  form:
+   { grant_type: 'password',
+     username: 'example3@example.com',
+     password: 'very_Strong1_password',
+     audience: 'https://dev-mpo4a74t24cr54of.us.auth0.com/api/v2/',
+     scope: 'offline_access',
+     client_id: 'dG9RzB82qEj9lSYCjgsGUWikomA1mmUc',
+     client_secret: 'POkuM9El6ORqQXTShZBnC6dmhNifWvEPUDPGE2Zbqfe1waWcqMmXeNgfFo74nwO6' }
+   };
+
+
+let refresh_token = (token) =>{
+    request({ method: 'POST',
+        url: 'https://dev-mpo4a74t24cr54of.us.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/json'},
+        body: JSON.stringify({
+            grant_type: 'refresh_token',
+            client_id: 'dG9RzB82qEj9lSYCjgsGUWikomA1mmUc',
+            client_secret: 'POkuM9El6ORqQXTShZBnC6dmhNifWvEPUDPGE2Zbqfe1waWcqMmXeNgfFo74nwO6',
+            refresh_token: token
+        })
+    }, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        console.log(JSON.parse(body));
+      });
+}
+
+  
+request(user_token_options, function (error, response, body) {
+    if (error) throw new Error(error);
+    token = JSON.parse(body)['refresh_token'];
+    refresh_token(token)
+});
